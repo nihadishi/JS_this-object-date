feaa483babf53744f5d7d82ae40612b58e39e6f9
Nihad <nihadishi@gmail.com>
2023-04-27 15:53:07 +0400

    6

--- object-task5.js
+++ object-task5.js
@@ -7,19 +7,47 @@ const todaysWinner = {
 }
  
 const winnerApplicants = {
-    '001': {
+    1: {
         name: 'Max',
         age: 25
     },
-    '201': {
+    2: {
         name: 'Svetlana',
         age: 20
     },
-    '304': {
+    3: {
         name: 'Catherine',
         age: 35
     },
+    4: {
+        name: 'asadaf',
+        age: 35
+    },
+    5: {
+        name: 'Catsdgsherine',
+        age: 35
+    },
+    6: {
+        name: 'sdgdfhgsv',
+        age: 35
+    },
+    7: {
+        name: 'Cathjhgfdserine',
+        age: 35
+    },
+    8: {
+        name: 'qwerqrw',
+        age: 35
+    },
 }
- 
+ function getWinner(winnerApplicants,todaysWinner){
+    let randomNumb = getRandomNumberInRange(1, Object.keys(winnerApplicants).length);
+    console.log(randomNumb);
+    // winnerApplicants[randomNumb].todaysWinner = todaysWinner;
+    console.log(winnerApplicants[randomNumb]);
+
+ }
 const resultWinner = getWinner(winnerApplicants, todaysWinner);
-console.log('resultWinner', resultWinner); // { prize: '$10,000', name: 'Maxim', age: 25 }
+console.log(todaysWinner,resultWinner); // { prize: '$10,000', name: 'Maxim', age: 25 }
+
+
---
+++ object-task6.js
@@ -0,0 +1,7 @@
+const ordersArr = [4, 2, 1, 3];
+const people = [
+    { id: 1, name: "Maxim" },
+    { id: 2, name: "Nicholas" },
+    { id: 3, name: "Angelina" },
+    { id: 4, name: "Vitaly" },
+];
